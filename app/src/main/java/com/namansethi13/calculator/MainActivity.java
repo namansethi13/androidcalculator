308fb9855cd22174e54608bbebd9c7d031fb4583
stinger13 <93597591+stinger13@users.noreply.github.com>
2023-05-30 01:10:18 +0530

    trying

--- app/src/main/java/com/namansethi13/calculator/MainActivity.java
+++ app/src/main/java/com/namansethi13/calculator/MainActivity.java
@@ -24,17 +24,15 @@ public class MainActivity extends AppCompatActivity {
                 number = 0;
                 sign = -1;
             }else if(c == '('){
-                //we push the result first, then sign;
                 stack.push(result);
                 stack.push(sign);
-                //reset the sign and result for the value in the parenthesis
                 sign = 1;
                 result = 0;
             }else if(c == ')'){
                 result += sign * number;
                 number = 0;
-                result *= stack.pop();    //stack.pop() is the sign before the parenthesis
-                result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
+                result *= stack.pop();
+                result += stack.pop();
 
             }
         }
